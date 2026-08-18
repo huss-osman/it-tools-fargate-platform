099836d2203a25fe3229a0570be0d5540fe2df34
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 00:30:11 +0100

    docs: add Terraform variable descriptions

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,24 +1,29 @@
 variable "vpc_cidr_block" {
-  type    = string
-  default = "10.0.0.0/22"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/22"
 }
 
 variable "public_subnet_cidr_blocks" {
-  type    = list(string)
-  default = ["10.0.0.0/25", "10.0.1.0/25"]
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.0.0/25", "10.0.1.0/25"]
 }
 
 variable "private_subnet_cidr_blocks" {
-  type    = list(string)
-  default = ["10.0.2.0/25", "10.0.3.0/25"]
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.2.0/25", "10.0.3.0/25"]
 }
 
 variable "availability_zones" {
-  type    = list(string)
-  default = ["eu-west-2a", "eu-west-2b"]
+  description = "Availability Zones used for the Multi-AZ deployment"
+  type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
 }
 
 variable "default_cidr_block" {
-  type    = string
-  default = "0.0.0.0/0"
+  description = "Default CIDR block used for internet-facing traffic"
+  type        = string
+  default     = "0.0.0.0/0"
 }
