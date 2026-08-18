9de8bf84459b6f74c0bef8918a4dcc515908e3df
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 00:31:57 +0100

    docs: add ACM variable descriptions

--- infra/modules/acm/variables.tf
+++ infra/modules/acm/variables.tf
@@ -1,7 +1,9 @@
 variable "domain_name" {
-  type = string
+  description = "Domain name used for the ACM certificate"
+  type        = string
 }
 
 variable "route53_zone_id" {
-  type = string
+  description = "Route 53 hosted zone ID used for DNS validation"
+  type        = string
 }
