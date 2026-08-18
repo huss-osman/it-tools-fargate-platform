d41cb494c05960fae7afbf6bdfee0f0770e4d7f0
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 00:33:43 +0100

    docs: add ECS variable descriptions

--- infra/modules/ecs/variables.tf
+++ infra/modules/ecs/variables.tf
@@ -1,34 +1,42 @@
 variable "vpc_id" {
-  type = string
+  description = "ID of the VPC where the ECS service is deployed"
+  type        = string
 }
 
 variable "private_subnet_ids" {
-  type = list(string)
+  description = "IDs of the private subnets used by ECS tasks"
+  type        = list(string)
 }
 
 variable "alb_security_group_id" {
-  type = string
+  description = "Security group ID of the Application Load Balancer"
+  type        = string
 }
 
 variable "target_group_arn" {
-  type = string
+  description = "ARN of the ALB target group used by the ECS service"
+  type        = string
 }
 
 variable "ecr_repository_url" {
-  type = string
+  description = "URL of the ECR repository containing the application image"
+  type        = string
 }
 
 variable "app_port" {
-  type    = number
-  default = 8080
+  description = "Port exposed by the application container"
+  type        = number
+  default     = 8080
 }
 
 variable "task_cpu" {
-  type    = number
-  default = 256
+  description = "CPU units allocated to the Fargate task"
+  type        = number
+  default     = 256
 }
 
 variable "task_memory" {
-  type    = number
-  default = 512
+  description = "Memory allocated to the Fargate task in MiB"
+  type        = number
+  default     = 512
 }
