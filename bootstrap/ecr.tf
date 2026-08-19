892c595d6ea4d8adb723cef692d77696be61846d
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 17:17:10 +0100

    feat: add ECR image lifecycle policy

--- bootstrap/ecr.tf
+++ bootstrap/ecr.tf
@@ -7,3 +7,23 @@ resource "aws_ecr_repository" "app" {
   }
 }
 
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep the most recent 10 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
