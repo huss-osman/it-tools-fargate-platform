daa37854651a601c4b5e70eaa335074b0d4dd8bc
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 23:56:39 +0100

    fix: prevent forced ECR repository deletion

--- bootstrap/ecr.tf
+++ bootstrap/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "app" {
   name                 = "it-tools-fargate"
   image_tag_mutability = "MUTABLE"
+  force_delete         = false
 
   image_scanning_configuration {
     scan_on_push = true
