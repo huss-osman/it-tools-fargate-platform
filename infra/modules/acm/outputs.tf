1f54faec6b036bd2987d2d9c3c37139a3f76cdab
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 23:54:56 +0100

    docs: add ACM module output description

--- infra/modules/acm/outputs.tf
+++ infra/modules/acm/outputs.tf
@@ -1,3 +1,4 @@
 output "certificate_arn" {
-  value = aws_acm_certificate_validation.main.certificate_arn
+  description = "ARN of the validated ACM certificate used for HTTPS"
+  value       = aws_acm_certificate_validation.main.certificate_arn
 }
