0985340afeea1540041503b5ae8c066cc05120e0
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 23:52:56 +0100

    docs: add ECS module output descriptions

--- infra/modules/ecs/outputs.tf
+++ infra/modules/ecs/outputs.tf
@@ -1,15 +1,19 @@
 output "cluster_name" {
-  value = aws_ecs_cluster.main.name
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
 }
 
 output "service_name" {
-  value = aws_ecs_service.app.name
+  description = "Name of the ECS service running the application"
+  value       = aws_ecs_service.app.name
 }
 
 output "ecs_security_group_id" {
-  value = aws_security_group.ecs.id
+  description = "ID of the security group attached to the ECS tasks"
+  value       = aws_security_group.ecs.id
 }
 
 output "task_definition_arn" {
-  value = aws_ecs_task_definition.app.arn
+  description = "ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.app.arn
 }
