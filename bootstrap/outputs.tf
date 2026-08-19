f20ba335a1b95a373322412225612130526cc597
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 17:35:42 +0100

    docs: add bootstrap output descriptions

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,7 +1,9 @@
 output "github_actions_role_arn" {
-  value = aws_iam_role.github_actions.arn
+  description = "ARN of the IAM role assumed by GitHub Actions through OIDC"
+  value       = aws_iam_role.github_actions.arn
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.app.repository_url
+  description = "URL of the ECR repository used to store application images"
+  value       = aws_ecr_repository.app.repository_url
 }
