00790cb373d43c1979beb285ec65ac02021803d7
huss-osman <osmanhusseinl@hotmail.com>
2026-08-20 00:00:43 +0100

    feat: add ECS health check grace period

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -105,6 +105,7 @@ resource "aws_ecs_service" "app" {
 
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
+  health_check_grace_period_seconds  = 60
 
   deployment_circuit_breaker {
     enable   = true
