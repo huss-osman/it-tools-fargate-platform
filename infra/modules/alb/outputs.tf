a2ae4c2231046b8020bfe6b248e5ada2661f04c1
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 23:51:59 +0100

    docs: add ALB module output descriptions

--- infra/modules/alb/outputs.tf
+++ infra/modules/alb/outputs.tf
@@ -1,15 +1,19 @@
 output "alb_dns_name" {
-  value = aws_lb.main.dns_name
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.main.dns_name
 }
 
 output "alb_zone_id" {
-  value = aws_lb.main.zone_id
+  description = "Canonical hosted zone ID of the Application Load Balancer"
+  value       = aws_lb.main.zone_id
 }
 
 output "alb_security_group_id" {
-  value = aws_security_group.alb.id
+  description = "ID of the security group attached to the Application Load Balancer"
+  value       = aws_security_group.alb.id
 }
 
 output "target_group_arn" {
-  value = aws_lb_target_group.app.arn
+  description = "ARN of the target group used by the ECS service"
+  value       = aws_lb_target_group.app.arn
 }
