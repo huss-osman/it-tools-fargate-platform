b84765cd0283c1b1e7de6663d533b3aaf882a53f
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 23:50:18 +0100

    docs: add VPC module output descriptions

--- infra/modules/vpc/outputs.tf
+++ infra/modules/vpc/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "ID of the VPC created by this module"
+  value       = aws_vpc.main.id
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+  description = "IDs of the public subnets created across Availability Zones"
+  value       = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
+  description = "IDs of the private subnets created across Availability Zones"
+  value       = aws_subnet.private[*].id
 }
