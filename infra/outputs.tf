5da76c09e867477d1ba53fea922d03bf027ff470
huss-osman <osmanhusseinl@hotmail.com>
2026-08-19 23:48:44 +0100

    docs: add infrastructure output descriptions

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "ID of the VPC hosting the application infrastructure"
+  value       = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
+  description = "IDs of the public subnets used by internet-facing resources"
+  value       = module.vpc.public_subnet_ids
 }
 
 output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
+  description = "IDs of the private subnets used by ECS tasks"
+  value       = module.vpc.private_subnet_ids
 }
